951dd29bf7599c7951301a98c138bf9e0a045ece
Jaakko Mursu <jaakko.mursu@f-solutions.fi>
2022-03-14 16:17:42 +0200

    Add course class model

---
+++ loppuproj/src/main/java/com/project/loppuproj/DataModels/Course.java
@@ -0,0 +1,50 @@
+package com.project.loppuproj.DataModels;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Course {
+    private String subject;
+    private String teacher;
+    private int roomNum;
+    ArrayList<UUID> students = new ArrayList<>();
+    protected UUID classId = UUID.randomUUID();
+
+    public Course(String subject, String teacher, int roomNum) {
+        this.subject = subject;
+        this.teacher = teacher;
+        this.roomNum = roomNum;
+    }
+
+    public Course(String subject, String teacher, int roomNum, UUID classId) {
+        this.subject = subject;
+        this.teacher = teacher;
+        this.roomNum = roomNum;
+        this.classId = classId;
+    }
+
+    public String getTeacher() {
+        return this.teacher;
+    }
+
+    public String getSubject() {
+        return this.subject;
+    }
+
+    public int getLocation() {
+        return this.roomNum;
+    }
+
+    public void addStudent(UUID studentId) {
+        this.students.add(studentId);
+    }
+
+    public ArrayList<UUID> getStudents() {
+        return this.students;
+    }
+
+    public UUID getUUID() {
+        return this.classId;
+    }
+
+}
