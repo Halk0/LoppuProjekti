dbce20e19dd7b23e4ec04eacc51c58e61388d254
Jaakko Mursu <jaakko.mursu@f-solutions.fi>
2022-03-15 14:07:17 +0200

    Moved state service to proper path with bugfixes
    Fixed a bug where springboot could not init class due to missing
    initializer. Allso a bug was fixed where extra conversion from String to
    JSONObject was made.
    State files are now allso handled so that springboot finds them.
    Courses should now be added properly to current state from saved file

--- loppuproj/src/main/java/com/project/loppuproj/service/StateService.java
+++ loppuproj/src/main/java/com/project/loppuproj/service/StateService.java
@@ -1,5 +1,6 @@
-package com.project.loppuproj.services;
+package com.project.loppuproj.service;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -8,32 +9,44 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import com.project.loppuproj.DataModels.Student;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.loppuproj.DataModels.Course;
+import com.project.loppuproj.data.Course;
+import com.project.loppuproj.data.Student;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StateService {
 
+    File studStateFile;
+    File courStateFile;
+
+    public StateService() {
+        try {
+            this.studStateFile = new ClassPathResource("static/StudentState.json").getFile();
+            this.courStateFile = new ClassPathResource("static/KurssiState.json").getFile();
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+
     public List<Student> readStudState() throws FileNotFoundException {
         List<Student> studentit = new ArrayList<>();
         JSONParser parseri = new JSONParser();
         try {
-            JSONArray obj = (JSONArray) parseri.parse(new FileReader("./OpiskelijaState.json"));
+            JSONArray obj = (JSONArray) parseri.parse(new FileReader(studStateFile));
             for (Object o : obj) {
                 JSONObject student = (JSONObject) o;
                 Student studentti = new Student(student.get("name").toString(), student.get("email").toString(),
                         UUID.fromString(student.get("identifier").toString()));
                 studentti.addOsp(Integer.parseInt(student.get("osp").toString()));
-                JSONArray kurssit = (JSONArray) student.get("Courses");
-                for (Object obje : kurssit) {
-                    JSONObject opiskKurssi = (JSONObject) obje;
-                    studentti.addToCourse(UUID.fromString(opiskKurssi.toString()));
+                JSONArray kursseja = (JSONArray) student.get("courses");
+                for (Object obje : kursseja) {
+                    studentti.addToCourse(UUID.fromString(obje.toString()));
                 }
                 studentit.add(studentti);
             }
@@ -47,17 +60,17 @@ public class StateService {
         List<Course> kurssit = new ArrayList<>();
         JSONParser parseri = new JSONParser();
         try {
-            JSONArray obj = (JSONArray) parseri.parse(new FileReader("./KurssiState.json"));
+            JSONArray obj = (JSONArray) parseri.parse(new FileReader(courStateFile));
             for (Object o : obj) {
                 JSONObject kurssi = (JSONObject) o;
-                Course course = new Course(kurssi.get("subject").toString(), kurssi.get("email").toString(),
+                Course course = new Course(kurssi.get("subject").toString(), kurssi.get("teacher").toString(),
                         Integer.parseInt(kurssi.get("roomNum").toString()),
                         UUID.fromString(kurssi.get("classId").toString()));
                 JSONArray oppilaat = (JSONArray) kurssi.get("students");
                 for (Object objop : oppilaat) {
-                    JSONObject kursOpisk = (JSONObject) objop;
-                    course.addStudent(UUID.fromString(kursOpisk.toString()));
+                    course.addStudent(UUID.fromString(objop.toString()));
                 }
+                kurssit.add(course);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -67,12 +80,12 @@ public class StateService {
 
     public void writeStudentState(List<Student> opiskelijat) throws IOException {
         ObjectMapper mapperi = new ObjectMapper();
-        mapperi.writeValue(new FileWriter("./OpiskelijaState.json"), opiskelijat);
+        mapperi.writeValue(new FileWriter(studStateFile), opiskelijat);
     }
 
     public void writeCourseState(List<Course> kurssit) throws IOException {
         ObjectMapper mapperi = new ObjectMapper();
-        mapperi.writeValue(new FileWriter("./KurssiState.json"), kurssit);
+        mapperi.writeValue(new FileWriter(courStateFile), kurssit);
     }
 
 }
