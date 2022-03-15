9479bd35d67ed4a4e55592a6b8b78b520be7362d
Jaakko Mursu <jaakko.mursu@f-solutions.fi>
2022-03-15 14:04:04 +0200

    Made metadata classes
    Metadata classes make typechecking for correct data in requet body easy

---
+++ loppuproj/src/main/java/com/project/loppuproj/data/CourseMeta.java
@@ -0,0 +1,13 @@
+package com.project.loppuproj.data;
+
+public class CourseMeta {
+    public String subject;
+    public String teacher;
+    public int roomNum;
+
+    CourseMeta(String subject, String teacher, int roomNum) {
+        this.subject = subject;
+        this.teacher = teacher;
+        this.roomNum = roomNum;
+    }
+}
---
+++ loppuproj/src/main/java/com/project/loppuproj/data/StudentMeta.java
@@ -0,0 +1,12 @@
+package com.project.loppuproj.data;
+
+public class StudentMeta {
+
+    public String name;
+    public String email;
+
+    StudentMeta(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
